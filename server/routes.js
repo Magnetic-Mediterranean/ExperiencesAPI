fc6a5516eec95c8a960ca1e7076d338b15fc0a70
Nilai Patel <nilaip96@gmail.com>
2021-04-07 12:30:36 -0700

    get to post

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { getActivities } = require('./controllers/amadeusRequests.js');
 
-router.get('/activities', getActivities);
+router.post('/activities', getActivities);
 
 module.exports = router;
